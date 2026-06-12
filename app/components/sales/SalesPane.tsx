1b6a29055a4efdb70d7e44a645addc846cb3f783
N/A <ruben.wood1@gmail.com>
2026-06-12 18:44:26 +0100

    insights

--- app/components/sales/SalesPane.tsx
+++ app/components/sales/SalesPane.tsx
@@ -7,16 +7,22 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import { Separator } from "@/components/ui/separator"
 
 
 function totalBooks(sales: Sale[]): number {
     return sales.reduce((total, sale) => total + sale.book_count, 0);
 }
-function totalRevenue(sales: Sale[]): number {
+function totalSaleValue(sales: Sale[]): number {
     return sales.reduce((total, sale) => total + sale.sale_value, 0);
 }
 function totalCost(sales: Sale[]): number {
     return sales.reduce((total, sale) => total + sale.cost, 0);
+}
+
+function highestTotalValueShop(sales: Sale[]): number | null {
+    const maxSale = sales.reduce((max, sale) => sale.sale_value > max.sale_value ? sale : max, sales[0]);
+    return maxSale ? maxSale.shop_id : null;
 }
 
 export function SalesPane(props: { sales: Sale[] }) {
@@ -26,7 +32,7 @@ export function SalesPane(props: { sales: Sale[] }) {
                 <TableCaption>List of your sales</TableCaption>
                 <TableHeader>
                     <TableRow>
-                        <TableHead className="font-bold">Shop Name</TableHead>
+                        <TableHead className="font-bold">Shop</TableHead>
                         <TableHead className="font-bold">Books Sold</TableHead>
                         <TableHead className="font-bold">Sale Value</TableHead>
                         <TableHead className="font-bold">Cost</TableHead>
@@ -46,11 +52,23 @@ export function SalesPane(props: { sales: Sale[] }) {
                     <TableRow>
                         <TableCell className="font-bold">Total</TableCell>
                         <TableCell className="font-bold">{totalBooks(props.sales)}</TableCell>
-                        <TableCell className="font-bold">£{totalRevenue(props.sales).toFixed(2)}</TableCell>
+                        <TableCell className="font-bold">£{totalSaleValue(props.sales).toFixed(2)}</TableCell>
                         <TableCell className="font-bold">£{totalCost(props.sales).toFixed(2)}</TableCell>
                     </TableRow>
                 </TableBody>
             </Table>
+            <Separator className="my-4" />
+            <p>Profit: £{(totalSaleValue(props.sales) - totalCost(props.sales)).toFixed(2)}</p>
+            <br/>
+            <p className="text-sm text-muted-foreground">
+                Highest value store (individual sale): 
+            </p>
+            <p className="text-sm text-muted-foreground">
+                Highest value store (total sale value): {highestTotalValueShop(props.sales) !== null ? highestTotalValueShop(props.sales) : "N/A"}
+            </p>
+            <p className="text-sm text-muted-foreground">
+                Highest value store (per book): 
+            </p>
         </div>
     );
 }
